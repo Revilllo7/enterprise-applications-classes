66f67ec4f059af4231ea476b2746c946fdb83647
Oliver Gruba <olivergruba7@gmail.com>
2025-12-03 19:20:47 +0100

    DAO implementation

---
+++ zad-8/src/main/java/com/techcorp/employee/dao/EmployeeDAO.java
@@ -0,0 +1,18 @@
+package com.techcorp.employee.dao;
+
+import com.techcorp.employee.model.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeDAO {
+	List<Employee> findAll();
+
+	Optional<Employee> findByEmail(String email);
+
+	void save(Employee employee);
+
+	void delete(String email);
+
+	void deleteAll();
+}
