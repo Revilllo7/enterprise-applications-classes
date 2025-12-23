991be92f52a95e7ad5d0f0a1e8844c77e3253cc0
Oliver Gruba <olivergruba7@gmail.com>
2025-12-23 14:38:17 +0100

    renamed from zad-3 to lab03

--- zad-1/src/main/analytics/util/EmployeeComparators.java
+++
@@ -1,9 +0,0 @@
-package main.analytics.util;
-
-import main.model.Employee;
-import java.util.Comparator;
-
-public class EmployeeComparators {
-    public static final Comparator<Employee> BY_LAST_NAME =
-            Comparator.comparing(employee -> employee.getFullName().split(" ")[1]);
-}
--- zad-2/src/main/java/exception/ApiException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-/**
- * Błędy związane z komunikacją z API / deserializacją.
- */
-public class ApiException extends Exception {
-    public ApiException(String message) { super(message); }
-    public ApiException(String message, Throwable cause) { super(message, cause); }
-}
